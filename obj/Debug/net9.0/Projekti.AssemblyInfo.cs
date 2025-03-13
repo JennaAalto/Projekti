86c669650bbf2f7f88e4fb6b721725aa00bfcea6
Jenna Aalto <amk1004260@hamk.student.fi>
2025-03-13 08:28:47 +0200

    Työaikaseuranta lisätty

--- obj/Debug/net9.0/Projekti.AssemblyInfo.cs
+++ obj/Debug/net9.0/Projekti.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekti")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2691c531f09022f19bcc7cd284794c9c48d5e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d54e30f9e4e91dab0e6dd08c217ca7bba53a697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekti")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekti")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
